f7f56a7930cefa3f85c551e5cedaae4d212e9e55
Alexey <dreamswave.dev@gmail.com>
2023-05-12 17:15:54 +0300

    feat: split scene into components

--- src/components/Luminary.tsx
+++ src/components/Luminary.tsx
@@ -1,5 +1,44 @@
-function Luminary() {
-  return <div className="luminary"></div>;
+import { animated, easings, useSpring } from "@react-spring/web";
+import { sceneConfig } from "../scenes";
+
+type LuminaryProps = {
+  glow?: string;
+};
+
+function Luminary({ glow }: LuminaryProps) {
+  const luminaryMovement = useSpring({
+    from: {
+      top: "150%",
+    },
+    to: [
+      {
+        top: "50%",
+        delay: 0,
+      },
+      {
+        top: "50%",
+        delay: sceneConfig.duration.luminary.atTop,
+      },
+      {
+        top: "150%",
+        delay: 0,
+      },
+    ],
+    config: {
+      duration: sceneConfig.duration.luminary.movement,
+      easing: easings.easeInOutQuad,
+    },
+    loop: true,
+  });
+  return (
+    <animated.div
+      className="luminary"
+      style={{
+        filter: glow && `drop-shadow(0 0 4rem ${glow})`,
+        ...luminaryMovement,
+      }}
+    ></animated.div>
+  );
 }
 
 export default Luminary;
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,48 +1,19 @@
 import { Scene as SceneType } from "../types";
-import { useSpring, animated, easings } from "@react-spring/web";
-import { sceneConfig } from "../scenes";
+import Luminary from "./Luminary";
+import Ocean from "./Ocean";
+import Sky from "./Sky";
 
 type SceneProps = {
   config: SceneType;
 };
 
 function Scene({ config }: SceneProps) {
-  const luminaryMovement = useSpring({
-    from: {
-      top: "150%",
-    },
-    to: [
-      {
-        top: "50%",
-        delay: 0,
-      },
-      {
-        top: "50%",
-        delay: sceneConfig.duration.luminary.atTop,
-      },
-      {
-        top: "150%",
-        delay: 0,
-      },
-    ],
-    config: {
-      duration: sceneConfig.duration.luminary.movement,
-      easing: easings.easeInOutQuad,
-    },
-    loop: true,
-  });
   return (
     <div className="scene">
-      <div className="sky">
-        <animated.div
-          className="luminary"
-          style={{
-            filter: `drop-shadow(0 0 4rem ${config.luminary.glow})`,
-            ...luminaryMovement,
-          }}
-        ></animated.div>
-      </div>
-      <div className="ocean"></div>
+      <Sky>
+        <Luminary glow={config.luminary.glow} />
+      </Sky>
+      <Ocean />
     </div>
   );
 }
