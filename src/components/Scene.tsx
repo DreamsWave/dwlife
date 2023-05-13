dcbd6d408efccdfa8bf05acacc4988343367bf27
Alexey <dreamswave.dev@gmail.com>
2023-05-13 19:29:58 +0300

    feat: first working following

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,8 +1,11 @@
+import { animated, easings, useSpring } from "@react-spring/web";
 import { Scene as SceneType } from "../types";
 import { generateClouds } from "../utils";
 import Clouds from "./Clouds";
 import Luminary from "./Luminary";
 import Sky from "./Sky";
+import { sceneConfig } from "../scenes";
+import { useEffect, useRef, useState } from "react";
 
 type SceneProps = {
   config: SceneType;
@@ -10,9 +13,53 @@ type SceneProps = {
 
 function Scene({ config }: SceneProps) {
   const clouds = generateClouds();
+  const luminaryRef = useRef(null);
+  const [backgroundGradientPosition, setBackgroundGradientPosition] =
+    useState("150%");
+  const luminaryMovement = useSpring({
+    from: {
+      top: "150%",
+    },
+    to: [
+      {
+        top: "50%",
+        delay: 0,
+      },
+      {
+        top: "50%",
+        delay: sceneConfig.duration.luminary.atTop,
+      },
+      {
+        top: "150%",
+        delay: 0,
+      },
+    ],
+    config: {
+      duration: sceneConfig.duration.luminary.movement,
+      easing: easings.easeInOutQuad,
+    },
+    loop: true,
+  });
+
   const SkyComponent = ({ isReflexion = false }: { isReflexion?: boolean }) => (
-    <Sky isReflexion={isReflexion}>
-      <Luminary glow={config.luminary.glow} />
+    <Sky
+      isReflexion={isReflexion}
+      backgroundGradientPosition={backgroundGradientPosition}
+    >
+      {/* <Luminary
+        glow={config.luminary.glow}
+        luminaryMovement={luminaryMovement}
+      /> */}
+      <animated.div
+        id="luminary"
+        className="luminary"
+        style={{
+          filter:
+            config.luminary.glow &&
+            `drop-shadow(0 0 4rem ${config.luminary.glow})`,
+          ...luminaryMovement,
+        }}
+      ></animated.div>
       <Clouds isReflexion={isReflexion} clouds={clouds} />
     </Sky>
   );
