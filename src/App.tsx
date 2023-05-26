f2690bf53e06c0f7448bc0c0ca96837737266d75
Alexey <idreamzzer@gmail.com>
2023-05-26 12:21:50 +0300

    feat: basic scene transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { animated, useTransition } from "@react-spring/web";
 import Scene from "./components/Scene";
 import { scenes } from "./scenes";
+import { useState } from "react";
 
 function App() {
+  const [index, set] = useState(0);
+  const transitions = useTransition(index, {
+    key: index,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0, delay: 20000 },
+    config: { duration: 3000 },
+    onRest: (_a, _b, item) => {
+      if (index === item) {
+        set((state) => (state + 1) % scenes.length);
+      }
+    },
+    exitBeforeEnter: true,
+    loop: true,
+  });
   return (
     <div className="App">
-      {/* {scenes.map((scene, i) => (
-        <Scene key={i} config={scene} />
-      ))} */}
-      <Scene config={scenes[1]} />
+      {transitions((style, i) => (
+        <animated.div style={style}>
+          <Scene config={scenes[i]} />
+        </animated.div>
+      ))}
     </div>
   );
 }
